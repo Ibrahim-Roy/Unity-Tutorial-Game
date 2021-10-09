3e404ff2f1846ba71cf3f2d3295a3974a80eb36f
Ibrahim-Roy <ibrahimbadarroy@gmail.com>
2021-10-09 17:00:47 +0100

    Prefabs and PickUps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
+    private int count;
+
+    void Start() {
+        count = 0;
+    }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
@@ -17,4 +23,13 @@ public class PlayerController : MonoBehaviour
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
+
+    void OnTriggerEnter(Collider other) {
+            if (other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            count++;
+            print(count);
+        }
+    }
+
 }
